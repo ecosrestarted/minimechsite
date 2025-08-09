4b76619dc00ede8dae2d38dbe8dd4cb1516155ea
ecosrestarted <yarnobaert@gmail.com>
2025-08-09 16:40:57 +0200

    upate

--- main.js
+++
@@ -1,9 +0,0 @@
-// voorbeeld: simpele alert op contact form submit (vervang of breid uit zoals je wilt)
-const contactForm = document.getElementById('contactForm');
-if(contactForm){
-  contactForm.addEventListener('submit', e => {
-    e.preventDefault();
-    alert("Bericht ontvangen. We nemen snel contact met je op.");
-    contactForm.reset();
-  });
-}
